789eb572ecfbb4544bcbf5324033f695373e9c7a
20 <diademistv@gmail.com>
2024-06-22 12:46:09 +0300

    DI annotation add

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,67 +1,57 @@
 package ru.netology.servlet;
 
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.AppConfig;
 
 public class MainServlet extends HttpServlet {
   private PostController controller;
 
   @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+  public void init() throws ServletException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    controller = context.getBean(PostController.class);
+    SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, getServletContext());
   }
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    // если деплоились в root context, то достаточно этого
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
-
-      switch (method) {
-        case "GET":
-          if (path.equals("/api/posts")) {
-            controller.all(resp);
-          } else if (path.matches("/api/posts/\\d+")) {
-            final var id = parseIdFromPath(path);
-            controller.getById(id, resp);
-          } else {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-          }
-          break;
-        case "POST":
-          if (path.equals("/api/posts")) {
-            controller.save(req.getReader(), resp);
-          } else {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-          }
-          break;
-        case "DELETE":
-          if (path.matches("/api/posts/\\d+")) {
-            final var id = parseIdFromPath(path);
-            controller.removeById(id, resp);
-          } else {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-          }
-          break;
-        default:
-          resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-          break;
+      // primitive routing
+      if (method.equals("GET") && path.equals("/api/posts")) {
+        controller.all(resp);
+        return;
       }
+      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals("POST") && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     } catch (Exception e) {
       e.printStackTrace();
       resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
-  }
-
-  private long parseIdFromPath(String path) {
-    return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
   }
 }
